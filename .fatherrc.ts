1d594842abd04690faff47beaefd957c5388ad98
monkindey <monkindey@163.com>
2020-08-05 11:03:20 +0800

    reading dir to filter basic pkgs

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,16 +1,12 @@
 import * as fs from 'fs';
 
-const isPlugin = (pkg) => {
-  const names = pkg.split('-');
-  return names[names.length - 1] === 'plugin';
-};
-
-const packages = fs.readdirSync('./packages').filter(isPlugin);
+const basicPkgs = ['use-query-display', 'use-table', 'use-form-table'];
+const packages = fs.readdirSync('./packages').filter((pkg) => !basicPkgs.includes(pkg));
 
 export default {
   esm: 'babel',
   cjs: 'babel',
   disableTypeCheck: true,
   // 这个重要，涉及到 build 的时候顺序
-  pkgs: ['use-query-display', 'use-table', 'use-form-table', 'next-form-table'].concat(packages),
+  pkgs: basicPkgs.concat(packages),
 };
