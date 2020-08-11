45f15cada0196c01910592bcc0ae8e69fc1e34ea
monkindey <monkindey@163.com>
2020-08-11 11:50:05 +0800

    getParams try catch

--- packages/use-table/src/props/params.ts
+++ packages/use-table/src/props/params.ts
@@ -1,7 +1,14 @@
 import { Obj } from '../type';
 
 const useParams = (ctx: Obj) => {
-  ctx.getParams = ctx.props.getParams || ctx.store.paramMap.get;
+  const getParams = ctx.props.getParams || ctx.store.paramMap.get;
+  ctx.getParams = () => {
+    try {
+      return getParams();
+    } catch (err) {
+      return {};
+    }
+  };
 };
 
 export default useParams;
