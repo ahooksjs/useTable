9a21f6f39c6b8357d9121d488b038093ee4e38ad
monkindey <monkindey@163.com>
2020-12-27 15:08:23 +0800

    不然解构的时候会报 warning https://stackoverflow.com/questions/56151661/omit-property-variable-when-using-object-destructuring

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
   },
   env: {
     node: true,
